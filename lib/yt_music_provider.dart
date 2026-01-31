c9aadfc972ee0a3e3e0a75dd41cfc3bc7a5c1f92
Sheikh Haziq <sheikhhaziq76@gmail.com>
2026-01-31 21:37:24 +0530

    fixed limit not working

--- lib/yt_music_provider.dart
+++ lib/yt_music_provider.dart
@@ -54,7 +54,6 @@ class YtMusicProvider extends MusicProvider {
 
   @override
   Future<BrowseResult> browse(BrowsePayload? payload, {int limit = 1}) async {
-    final limit = 1;
     if (!initialized) {
       throw StateError('Provider not initialized');
     }
